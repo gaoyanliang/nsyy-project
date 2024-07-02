6816b313618b2fc51c949747e8e5c2a0ef28e0cf
gaoyanliang <yanliang2025@163.com>
2024-07-02 09:55:27 +0800

    fix gitignore

--- gyltest.py
+++ gyltest.py
@@ -12,6 +12,7 @@ import xlrd
 
 from gylmodules import global_config
 from gylmodules.composite_appointment import appt_config
+from gylmodules.critical_value.critical_value import call_third_systems_obtain_data
 from gylmodules.critical_value import cv_config
 from gylmodules.utils.db_utils import DbUtil
 from suds.client import Client
@@ -22,122 +23,121 @@ from zeep.transports import Transport
 import logging
 import requests
 
-data = [
-    {'id': 34, 'appt_id': 273, 'pay_id': 'Y0589907', 'advice_desc': '静脉注射(或静脉采血); 真空采血管(特殊采血管）',
-     'dept_id': 144, 'dept_name': '医学检验科', 'price': 6.8000, 'state': 0},
-    {'id': 35, 'appt_id': 273, 'pay_id': 'Y0589911',
-     'advice_desc': '梅毒螺旋体特异抗体测定; 丙型肝炎抗体测定(Anti--HCV); 人免疫缺陷病毒抗体测定(Anti-HIV)指金标法、硒标法; 乙型肝炎表面抗原测定(HBsAg)',
-     'dept_id': 144, 'dept_name': '医学检验科', 'price': 65.3000, 'state': 0},
-    {'id': 36, 'appt_id': 274, 'pay_id': 'Y0589909', 'advice_desc': '还少胶囊', 'dept_id': 245, 'dept_name': '门诊药房',
-     'price': 40.3000, 'state': 0},
-    {'id': 37, 'appt_id': 275, 'pay_id': 'Y0589912', 'advice_desc': 'X线计算机体层(CT)扫描(16-40层)', 'dept_id': 68643,
-     'dept_name': '16排CT', 'price': 203.0000, 'state': 0},
-    {'id': 38, 'appt_id': 276, 'pay_id': 'Y0589906', 'advice_desc': '精液质量与功能分析', 'dept_id': 94683,
-     'dept_name': '生殖男科门诊', 'price': 85.5000, 'state': 0},
-    {'id': 39, 'appt_id': 276, 'pay_id': 'Y0589908', 'advice_desc': '阴茎超声血流图检查', 'dept_id': 94683,
-     'dept_name': '生殖男科门诊', 'price': 118.3200, 'state': 0}
-]
+# data = {'msg_list': [{'socket_data': {
+#     "type": 400,
+#     "data": {
+#         "title": "危急值上报反馈",
+#         "context": "test"
+#     }},
+#     'pers_id': int(9797)}]}
+# headers = {'Content-Type': 'application/json'}
+# response = requests.post(global_config.socket_push_url, data=json.dumps(data), headers=headers)
+# print("Socket Push Status: ", response.status_code, "Response: ", response.text, "socket_data: ", data, "user_id: ")
 
-# 用于存储拼接后的结果
-result = {}
+# data = call_third_systems_obtain_data('send_wx_msg', {
+#     "type": "send_wx_msg",
+#     "key_d": {"type": 71, "process_id": 11527, "action": 4, "title": "危急值上报反馈", "content": "msg"},
+#     "randstr": "XPFDFZDF7193CIONS1PD7XCJ3AD4ORRC",
+#     "pers_id": 110100,
+#     "force_notice": 1})
+#
+# print(data)
 
-# 遍历数据
-for record in data:
-    appt_id = record['appt_id']
-    advice_desc = record['advice_desc']
 
-    if appt_id in result:
-        result[appt_id]['advice_desc'] += '; ' + advice_desc
-    else:
-        result[appt_id] = record.copy()
-        result[appt_id]['advice_desc'] = advice_desc
 
-# 转换结果为列表格式
-result_list = list(result.values())
 
-# 输出结果
-for r in result_list:
-    print(r)
+# json_data =  {'pat_no': '398709', 'pat_type': 3, 'record': {'id': 3271, 'cv_id': '8807', 'alertdt': datetime(2024, 7, 1, 14, 29, 29), 'state': 7, 'cv_source': 2, 'alertman': '2795', 'alertman_name': '张萌萌', 'alertman_pers_id': '110229', 'alert_dept_id': 144, 'alert_dept_name': '医学检验科', 'patient_type': 3, 'patient_treat_id': '398709', 'patient_name': '谢春香', 'patient_gender': '2', 'patient_age': '74岁', 'patient_phone': None, 'patient_bed_num': '65', 'req_docno': '袁苗', 'ward_id': 1001000, 'ward_name': '神经外科五病区护理单元', 'dept_id': 94143, 'dept_name': '神经外科五病区', 'cv_type': None, 'cv_name': '*钾（离子选择性电极）', 'cv_result': '2.7', 'cv_unit': 'mmol/L', 'cv_ref': '3.5-5.3', 'cv_flag': 'L', 'redo_flag': '2', 'alertrules': '<2.8000', 'handle_doctor_id': None, 'handle_doctor_name': None, 'handle_time': None, 'analysis': None, 'method': None, 'time': datetime(2024, 7, 1, 14, 29, 43), 'nurse_recv_id': None, 'nurse_recv_name': None, 'nurse_recv_time': None, 'nurse_recv_info': None, 'nurse_send_time': None, 'nursing_record': None, 'nursing_record_time': None, 'doctor_recv_id': 1034, 'doctor_recv_name': '乔路宽', 'doctor_recv_time': datetime(2024, 7, 1, 18, 11, 4), 'is_nurse_recv_timeout': 1, 'is_nurse_send_timeout': 0, 'is_doctor_recv_timeout': 1, 'is_doctor_handle_timeout': 0, 'is_timeout': 0, 'nurse_recv_timeout': 420, 'nurse_send_timeout': 60, 'doctor_recv_timeout': 300, 'doctor_handle_timeout': 120, 'total_timeout': 600}, 'handler_name': '乔路宽', 'timer': '2024-07-01 18:11:28', 'method': '补钾治疗', 'analysis': '低钾'}
+
+
+json_data =  {'pat_no': '394955', 'pat_type': 3, 'record': {'id': 3303, 'cv_id': '8824', 'alertdt': datetime(2024, 7, 2, 7, 3, 31), 'state': 7, 'cv_source': 2, 'alertman': '0601', 'alertman_name': '杨瀚', 'alertman_pers_id': '10323', 'alert_dept_id': 144, 'alert_dept_name': '医学检验科', 'patient_type': 3, 'patient_treat_id': '394955', 'patient_name': '聂子政', 'patient_gender': '1', 'patient_age': '73岁', 'patient_phone': None, 'patient_bed_num': '4', 'req_docno': '刘军', 'ward_id': 1001120, 'ward_name': 'CCU护理单元', 'dept_id': 1000421, 'dept_name': '麻醉重症监护病房(AICU)', 'cv_type': None, 'cv_name': '*钠（离子选择性电极）', 'cv_result': '161', 'cv_unit': 'mmol/L', 'cv_ref': '137-147', 'cv_flag': 'H', 'redo_flag': '2', 'alertrules': '>160.0000', 'handle_doctor_id': None, 'handle_doctor_name': None, 'handle_time': None, 'analysis': None, 'method': None, 'time': datetime(2024, 7, 2, 7, 4, 13), 'nurse_recv_id': None, 'nurse_recv_name': None, 'nurse_recv_time': None, 'nurse_recv_info': None, 'nurse_send_time': None, 'nursing_record': None, 'nursing_record_time': None, 'doctor_recv_id': 740, 'doctor_recv_name': '刘军', 'doctor_recv_time': datetime(2024, 7, 2, 7, 52, 21), 'is_nurse_recv_timeout': 1, 'is_nurse_send_timeout': 0, 'is_doctor_recv_timeout': 1, 'is_doctor_handle_timeout': 0, 'is_timeout': 0, 'nurse_recv_timeout': 420, 'nurse_send_timeout': 60, 'doctor_recv_timeout': 300, 'doctor_handle_timeout': 120, 'total_timeout': 600}, 'handler_name': '刘军', 'timer': '2024-07-02 07:54:16', 'method': '加强鼻饲温开水 200ml q4h', 'analysis': '患者食管癌术后，禁食水'}
+
+
+
+def medical_record_writing_back(json_data):
+    try:
+        sql = ''
+        pat_type = int(json_data.get('pat_type'))
+        pat_no = int(json_data.get('pat_no'))
+        if pat_type in (1, 2):
+            # 门诊/急诊
+            sql = f'select 病人ID as pid, NO as hid from 病人挂号记录 where 门诊号 = \'{pat_no}\' order by 登记时间 desc'
+        elif pat_type == 3:
+            # 住院
+            sql = f'select 病人id as pid, 主页id as hid from 病案主页 where 住院号 = \'{pat_no}\' order by 主页id desc '
+
+        param = {
+            "type": "orcl_db_read",
+            "db_source": "nshis",
+            "randstr": "XPFDFZDF7193CIONS1PD7XCJ3AD4ORRC",
+            "sql": sql
+        }
+        data = call_third_systems_obtain_data('orcl_db_read', param)
+        if data:
+            record = json_data.get('record')
+            body = ''
+            recv_time = record.get('time').strftime("%Y-%m-%d %H:%M:%S")
+            body = body + "于 " + recv_time + "接收到 " + str(record.get('alert_dept_name')) \
+                   + " 推送的危机值: [" + str(record.get('cv_name')) + "]"
+            body = body + " " + str(record.get('cv_result'))
+            if record.get('cv_unit'):
+                body = body + " " + record.get('cv_unit')
+
+            body = body + "医生 " + json_data.get('handler_name') + " " + json_data.get('timer') + "处理了该危机值"
+            if json_data.get('analysis'):
+                body = body + " 原因分析: " + json_data.get('analysis')
+            if json_data.get('method'):
+                body = body + " 处理方法: " + json_data.get('method')
+            pid = data[0].get('PID', 0)
+            hid = data[0].get('HID', 0)
+            param = {
+                "type": "his_procedure",
+                "procedure": "jk_p_Pat_List",
+                "病人id": pid,
+                "主页id": hid,
+                "内容": body,
+                "分类": "3",
+                "记录人": json_data.get('handler_name'),
+                "审核时间": json_data.get('timer'),
+                "医嘱名称": record.get('cv_name'),
+                "分类名": "危机值记录",
+                "标签说明": record.get('cv_name')
+            }
+            data = call_third_systems_obtain_data('his_procedure', param)
+            print(data)
+    except Exception as e:
+        print('病历回写异常：param = ', param, " json_data = ", json_data, " 异常信息 = ", e)
+
+
+medical_record_writing_back(json_data)
+
+
+
+
+
+
+
+
+
+
 
 #
-# # 示例 room_dict 数据结构
-# room_dict = {
-#     'room1': {
-#         '2024-06-11': {'1': {1: 2, 2: 2, 3: 2, 4: 2, 5: 2, 6: 2, 7: 2, 8: 2},
-#                        '2': {9: 2, 10: 2, 11: 2, 12: 2, 13: 2, 14: 2, 15: 2, 16: 2}},
-#         '2024-06-12': {'1': {1: 2, 2: 2, 3: 2, 4: 2, 5: 2, 6: 2, 7: 2, 8: 2},
-#                        '2': {9: 2, 10: 2, 11: 2, 12: 2, 13: 2, 14: 2, 15: 2, 16: 2}},
-#         '2024-06-13': {'1': {1: 2, 2: 2, 3: 2, 4: 2, 5: 2, 6: 2, 7: 2, 8: 2},
-#                        '2': {9: 2, 10: 2, 11: 2, 12: 2, 13: 2, 14: 2, 15: 2, 16: 2}},
-#     }
-# }
-#
-# periodd = {1: [1, 2, 3, 4, 5, 6, 7, 8], 2: [9, 10, 11, 12, 13, 14, 15, 16]}  # 1 上午 2 下午 3 全天
-# periodd[3] = periodd[1] + periodd[2]
-#
-# def check_appointment_quantity(book_info):
-#     def find_next_available(date, next_slot, period_list, find_in):
-#         capdict_am = room_dict[book_info['room']][date]['1'] if room_dict[book_info['room']][date].get('1') else {}
-#         capdict_pm = room_dict[book_info['room']][date]['2'] if room_dict[book_info['room']][date].get('2') else {}
-#         for s in period_list:
-#             if s <= 8 and find_in in (1, 3):  # 上午时段
-#                 if s >= next_slot and capdict_am.get(s, 0) > 0:
-#                     return date, s
-#             elif s > 8 and find_in in (2, 3):  # 下午时段
-#                 if s >= next_slot and capdict_pm.get(s, 0) > 0:
-#                     return date, s
-#         return None, None
-#
-#     room = book_info['room']
-#     book_date = book_info.get('date', None)
-#     period = int(book_info.get('period')) if book_info.get('period') else 3
-#
-#     current_slot = appt_config.appt_slot_dict[datetime.now().hour]
-#     # 如果指定了日期，直接在指定日期查找可用时段
-#     if book_date:
-#         next_date, next_slot = find_next_available(book_date, current_slot, periodd[period], period)
-#         if not next_slot:
-#             raise Exception("No available period found for the specified date and period")
-#         return next_date, next_slot
-#
-#     today = datetime.today().strftime("%Y-%m-%d")
-#     last_date = book_info.get('last_date', today)
-#     last_slot = book_info.get('last_slot', None)
-#     # 查找从last_date和last_slot之后可用的时间和时间段
-#     for date in sorted(room_dict[room].keys()):
-#         if last_date and date < last_date:
-#             continue
-#         if date == last_date:
-#             slot_to_check = last_slot + 1 if last_slot is not None else current_slot
-#             next_date, next_slot = find_next_available(date, slot_to_check, periodd[period], period)
-#         else:
-#             next_date, next_slot = find_next_available(date, 1, periodd[period], period)
-#
-#         if next_slot:
-#             return next_date, next_slot
-#
-#     # return None, "No available period found"
-#     raise Exception("No available period found")
+# data = {'msg_list': [{'socket_data': {
+#     "type": 400,
+#     "data": {
+#         "title": "危急值上报反馈",
+#         "context": "患者 xxx 的危急值，已通知相关科室"
+#     }},
+#     'pers_id': 10013}]}
 #
 #
-# date, period = check_appointment_quantity({
-#     'date': '2024-06-11',
-#     'room': 'room1',
-#     # 'last_date': '2024-06-12',  # 上一次预约的日期
-#     # 'last_slot': 10,             # 上一次预约的时间段
-#     'period': 2                # 本次预约的时段，1 表示上午
-# })
-# print(f"Date: {date}, Period: {period}")
+# headers = {'Content-Type': 'application/json'}
+# response = requests.post(global_config.socket_push_url, data=json.dumps(data), headers=headers)
+# print("Socket Push Status: ", response.status_code, "Response: ", response.text, "socket_data: ", data,
+#       "user_id: ", 110100)
 #
-# date, period = check_appointment_quantity({
-#     # 'date': '2024-06-11',
-#     'room': 'room1',
-#     'last_date': '2024-06-12',  # 上一次预约的日期
-#     'last_slot': 10,             # 上一次预约的时间段
-#     # 'period': 2                # 本次预约的时段，1 表示上午
-# })
-# print(f"Date: {date}, Period: {period}")
-#
+
+
+
 
 
 
