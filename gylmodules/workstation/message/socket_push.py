0290ac3bbece6c1cb4c6303fce69d47a28423ea9
gaoyanliang <yanliang2025@163.com>
2024-07-02 09:54:45 +0800

    fix  message socket url

--- gylmodules/workstation/message/socket_push.py
+++ gylmodules/workstation/message/socket_push.py
@@ -12,34 +12,21 @@ import json
 # from tools import socket_send
 # socket_send(socket_data, 'm_user', pers_id)
 
-# 测试环境推送地址
-socket_push_url = 'http://192.168.124.53:6080/inter_socket_msg'
-
 # 消息推送 type = 100
 
 
 def push(socket_data: dict, user_id: int):
     print('向用户 ' + str(user_id) + ' 推送消息: ' + json.dumps(socket_data, default=str))
 
-    if global_config.run_in_local:
-        # 测试环境
-        # 要发送的数据
-        data = {'msg_list': [{'socket_data': socket_data, 'pers_id': user_id}]}
-
-        # 设置请求头
-        headers = {'Content-Type': 'application/json'}
-
-        # 发送POST请求
-        response = requests.post(socket_push_url, data=json.dumps(data), headers=headers)
-
-        # 打印响应内容
-        print("Socket Push Status: ", response.status_code)
-        print("Socket Push Response: ", response.text)
-    else:
-        # 正式环境：
-        # from tools import socket_send
-        # socket_send(socket_data, 'm_user', pers_id)
-        print("正式环境 socket 推送")
-
+    # 测试环境
+    # 要发送的数据
+    data = {'msg_list': [{'socket_data': socket_data, 'pers_id': user_id}]}
+    # 设置请求头
+    headers = {'Content-Type': 'application/json'}
+    # 发送POST请求
+    response = requests.post(global_config.socket_push_url, data=json.dumps(data), headers=headers)
+    # 打印响应内容
+    print("Socket Push Status: ", response.status_code)
+    print("Socket Push Response: ", response.text)
 
 # push("test", 100)
