d3e145a34813eba2f2c5d38954e44b20a370330e
gaoyanliang <yanliang2025@163.com>
2024-11-08 09:30:05 +0800

    同一个患者当天的治疗记录 10 分钟内仅允许扣费一次

--- gylmodules/hyperbaric_oxygen_therapy/hbot_server.py
+++ gylmodules/hyperbaric_oxygen_therapy/hbot_server.py
@@ -529,6 +529,12 @@ def hbot_charge(json_data):
     tid = json_data.get('tid')
     pay_num = json_data.get('pay_num')
 
+    key = f"hbot:{rid}:{tid}"
+    redis_client = redis.Redis(connection_pool=pool)
+    # 尝试设置键，只有当键不存在时才设置成功, ex=600 表示过期时间 600 秒（10 分钟），nx=True 表示不存在时才设置
+    if not redis_client.set(key, pay_num, ex=600, nx=True):
+        raise Exception('扣费失败：当前患者今天的治疗，10分钟内已扣过一次费，请勿重复扣费')  # 已经在 10 分钟内执行过，不能重复执行
+
     db = DbUtil(global_config.DB_HOST, global_config.DB_USERNAME, global_config.DB_PASSWORD,
                 global_config.DB_DATABASE_GYL)
     query_sql = f"select * from nsyy_gyl.hbot_treatment_record where id = '{tid}' "
