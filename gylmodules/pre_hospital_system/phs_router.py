a973ee619fcf988c9a49192a776b8176415b4bad
gaoyanliang <yanliang2025@163.com>
2025-05-29 16:59:20 +0800

    上线院前系统

---
+++ gylmodules/pre_hospital_system/phs_router.py
@@ -0,0 +1,67 @@
+from flask import Blueprint, jsonify, request
+
+from gylmodules.global_tools import api_response, validate_params
+from gylmodules.pre_hospital_system import phs_server, phs_config
+
+phs = Blueprint('Pre hospital emergency system', __name__, url_prefix='/phs')
+
+
+@phs.route('/register', methods=['POST'])
+@api_response
+def patient_register(json_data):
+    return phs_server.patient_registration(json_data)
+
+
+@phs.route('/update_patient_info', methods=['POST'])
+@api_response
+@validate_params('register_id')
+def update_patient_info(json_data):
+    # 提取并处理数据
+    register_id = json_data.pop('register_id')
+    phs_server.update_patient_info(json_data, register_id)
+
+
+@phs.route('/query_patient_info', methods=['POST', 'GET'])
+@api_response
+def query_patient_info(json_data):
+    return phs_server.query_patient_info(json_data.get('register_id'), json_data.get('record_id'))
+
+
+@phs.route('/query_patient_list', methods=['POST', 'GET'])
+@api_response
+@validate_params('start_date', 'end_date')
+def query_patient_list(json_data):
+    return phs_server.query_patient_list(json_data.get('key'), json_data.get('start_date'),
+                                         json_data.get('end_date'), json_data.get('page_number'),
+                                         json_data.get('page_size'))
+
+
+@phs.route('/query_record_list', methods=['POST', 'GET'])
+@api_response
+def query_record_list(json_data):
+    return phs_server.query_record_list()
+
+
+@phs.route('/query_patient_record_list', methods=['POST', 'GET'])
+@api_response
+def query_patient_record_list(json_data):
+    return phs_server.query_patient_record_list(json_data.get('register_id'))
+
+
+@phs.route('/create_patient_record', methods=['POST', 'GET'])
+@api_response
+@validate_params('register_id', 'record_id')
+def create_patient_record(json_data):
+    return phs_server.create_patient_record(json_data.get('register_id'),
+                                            json_data.get('record_id'), json_data.get('record_data'))
+
+
+@phs.route('/car_no_list', methods=['POST', 'GET'])
+@api_response
+def car_no_list(json_data):
+    return phs_config.car_no_list
+
+
+
+
+
---
+++ gylmodules/pre_hospital_system/phs_server.py
@@ -0,0 +1,146 @@
+from gylmodules import global_config
+from gylmodules.utils.db_utils import DbUtil
+from datetime import datetime
+
+
+def patient_registration(json_data):
+    """
+    登记急救患者信息
+    :param json_data:
+    :return:
+    """
+    db = DbUtil(global_config.DB_HOST, global_config.DB_USERNAME, global_config.DB_PASSWORD,
+                global_config.DB_DATABASE_GYL)
+    insert_sql = f"INSERT INTO nsyy_gyl.phs_patient_registration ({','.join(json_data.keys())}) " \
+                 f"VALUES {str(tuple(json_data.values()))}"
+    last_rowid = db.execute(sql=insert_sql, need_commit=True)
+    if last_rowid == -1:
+        del db
+        raise Exception("急诊患者登记失败 sql = ", insert_sql)
+    del db
+
+
+def update_patient_info(json_data, register_id):
+    """
+    更新急救患者信息
+    :param json_data: 需要更新的字段及值的字典
+    :param register_id: 患者的唯一 ID
+    :return:
+    """
+    db = DbUtil(global_config.DB_HOST, global_config.DB_USERNAME, global_config.DB_PASSWORD,
+                global_config.DB_DATABASE_GYL)
+
+    # 生成 SET 部分的 SQL 语句
+    set_clause = ', '.join([f"{key} = %s" for key in json_data.keys()])
+
+    # 构造 SQL 语句
+    update_sql = f"UPDATE nsyy_gyl.phs_patient_registration SET {set_clause} WHERE id = %s"
+    params = tuple(json_data.values()) + (register_id,)
+    affected_rows = db.execute(update_sql, params, need_commit=True)
+
+    if affected_rows == 0:
+        del db
+        raise Exception(datetime.now(), f"急诊患者更新失败，未找到登记 ID = {register_id} 的记录。SQL = {update_sql}")
+
+    del db
+    return affected_rows
+
+
+def query_patient_info(register_id, record_id):
+    """
+    查询急救患者信息
+    :param register_id: 患者的唯一 ID
+    :param record_id:
+    :return:
+    """
+    db = DbUtil(global_config.DB_HOST, global_config.DB_USERNAME, global_config.DB_PASSWORD,
+                global_config.DB_DATABASE_GYL)
+
+    if record_id:
+        query_sql = f"SELECT * FROM nsyy_gyl.phs_record_data WHERE record_id = {int(record_id)}"
+    else:
+        query_sql = f"SELECT * FROM nsyy_gyl.phs_patient_registration WHERE id = {int(register_id)}"
+
+    data = db.query_all(query_sql)
+    del db
+
+    return data
+
+
+def query_patient_list(key, start_date, end_date, page_number, page_size):
+    """
+    查询急救患者列表
+    :param key:
+    :param start_date:
+    :param end_date:
+    :return:
+    """
+    db = DbUtil(global_config.DB_HOST, global_config.DB_USERNAME, global_config.DB_PASSWORD,
+                global_config.DB_DATABASE_GYL)
+
+    condition_sql = f"paichesj BETWEEN '{start_date}' AND '{end_date}' "
+    if key:
+        condition_sql = condition_sql + f"AND patient_name LIKE '%{key}%' "
+
+    query_sql = f"SELECT id, patient_name, patient_sex, patient_phone, chuzhen_addr, chubuzd, daoyuansj" \
+                f" FROM nsyy_gyl.phs_patient_registration WHERE {condition_sql}"
+    data = db.query_all(query_sql)
+    del db
+
+    total = len(data)
+    if page_number and page_size:
+        start_index = (page_number - 1) * page_size
+        end_index = start_index + page_size
+        data = data[start_index:end_index]
+
+    return {"list": data, "total": total}
+
+
+def query_record_list():
+    """
+    查询急救表单模版列表
+    :return:
+    """
+    db = DbUtil(global_config.DB_HOST, global_config.DB_USERNAME, global_config.DB_PASSWORD,
+                global_config.DB_DATABASE_GYL)
+    data = db.query_all("select * from nsyy_gyl.phs_record")
+    del db
+    return data
+
+
+def query_patient_record_list(register_id):
+    """
+    查询当前患者创建的急救表单列表
+    :return:
+    """
+    db = DbUtil(global_config.DB_HOST, global_config.DB_USERNAME, global_config.DB_PASSWORD,
+                global_config.DB_DATABASE_GYL)
+    data = db.query_all(f"select DISTINCT r.id, r.record_name, r.record_type from nsyy_gyl.phs_record r "
+                        f"join nsyy_gyl.phs_record_data rd on r.id = rd.record_id where rd.register_id = {register_id}")
+    del db
+    return data
+
+
+def create_patient_record(register_id, record_id, record_data):
+    """
+    创建患者表单
+    :param register_id:
+    :param record_id:
+    :param record_data:
+    :return:
+    """
+    db = DbUtil(global_config.DB_HOST, global_config.DB_USERNAME, global_config.DB_PASSWORD,
+                global_config.DB_DATABASE_GYL)
+    values = [(int(register_id), int(record_id), item.get('field_name'), item.get('field_value'),
+               item.get('field_type', '')) for item in record_data]
+
+    insert_sql = """INSERT INTO nsyy_gyl.phs_record_data (register_id, record_id, field_name, field_value, field_type)
+            VALUES (%s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE field_value = VALUES(field_value), 
+            field_type = VALUES(field_type)"""
+    last_row = db.execute_many(insert_sql, args=values, need_commit=True)
+    if last_row == -1:
+        del db
+        raise Exception("急救表单入库失败! ", insert_sql)
+    del db
+
+
