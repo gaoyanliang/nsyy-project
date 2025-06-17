041a515fa846bf22489445211e23d665de346d00
gaoyanliang <yanliang2025@163.com>
2025-06-17 09:53:51 +0800

    院内讲座系统改用logging

--- gylmodules/hospital_class/hosp_class_router.py
+++ gylmodules/hospital_class/hosp_class_router.py
@@ -1,10 +1,11 @@
 import json
-from datetime import datetime
+import logging
 from flask import Blueprint, jsonify, request
 
 from gylmodules.hospital_class import hosp_class_server
 
 hosp_class = Blueprint('hospital class', __name__, url_prefix='/hosp_class')
+logger = logging.getLogger(__name__)
 
 
 # 领导设计的接口，仅负责实现
@@ -15,7 +16,7 @@ def hosp_class_func():
         json_data = json.loads(request.get_data().decode('utf-8'))
         return hosp_class_server.hosp_class(json_data)
     except Exception as e:
-        print(datetime.now(), f"hosp_class_func Exception occurred: {e.__str__()}, param: ", json_data)
+        logger.error(f"hosp_class_func Exception occurred: {e.__str__()}, param: {json_data}")
         return jsonify({
             'code': 50000,
             'res': e.__str__()
